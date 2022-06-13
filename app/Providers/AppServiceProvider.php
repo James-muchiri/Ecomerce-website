f093d558f6ade42c8899d92d56f013e482983d83
dct-15 <jamesmuchiri1155@gmail.com>
2022-06-13 11:53:53 +0300

    gttf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
+        
         if (env('APP_ENV') != 'Production') {
             URL::forceScheme('https');
         } 
